326e4d8572414e355ef6a217e1af3206b90285ee
Syuzanna Manukyan <syuzmanukyan0201@gmail.com>
2022-02-21 01:07:50 +0400

    Home

---
+++ Lesson3.1/Program.cs
@@ -0,0 +1,80 @@
+﻿//1
+int a = 4441, b, c, d, e;
+b = a % 10;
+a /= 10;
+c = a % 10 * 10;
+a /= 10;
+d = a % 10 * 100;
+a /= 10;
+e = a % 10 * 1000;
+Console.WriteLine($"{e}+{d}+{c}+{b} = {e + d + c + b}");
+Console.ReadKey();
+
+//2
+string lastName = "Syuzanna";
+string firstName = "Manukyan";
+
+Console.WriteLine($"Es {lastName} {firstName}-n em ");
+Console.ReadKey();
+
+//3.1
+string a = Console.ReadLine();
+
+if (a == "erkushabti")
+    Console.WriteLine("1");
+else if (a == "ereqshabti")
+    Console.WriteLine("2");
+else if (a == "choreqshabti")
+    Console.WriteLine("3");
+else if (a == "hingshabti")
+    Console.WriteLine("4");
+else if (a == "urbat")
+    Console.WriteLine("5");
+else if (a == "shabat")
+    Console.WriteLine("6");
+else if (a == "kiraki")
+    Console.WriteLine("7");
+
+Console.ReadKey();
+
+//3.2
+int a = 25; float b = 5; byte c = 235; long d = -862;
+int k;
+if (a < b)
+{
+    k = a;
+    a = (int)b;
+    b = k;
+}
+if (a < c)
+{
+    k = a;
+    a = (int)c;
+    c = (byte)k;
+}
+if (a < d)
+{
+    k = a;
+    a = (int)d;
+    d = k;
+}
+if (b < c)
+{
+    k = (int)b;
+    b = c;
+    c = (byte)k;
+}
+if (b < d)
+{
+    k = (int)b;
+    b = d;
+    d = k;
+}
+if (c < d)
+{
+    k = c;
+    c = (byte)d;
+    d = k;
+}
+Console.WriteLine($"{a},{b},{c},{d}");
+Console.ReadKey();
---
+++ Lesson3.2/Program.cs
@@ -0,0 +1,320 @@
+﻿
+//21
+int a = 2, b = 7, c = -2, k;
+int d = Math.Max(a, b);
+int e = Math.Max(d, c);
+Console.WriteLine(e);
+
+//22
+int a = 2, b = 7, c = -2;
+int f = Math.Min(a, b);
+int g = Math.Min(f, c);
+Console.WriteLine(g);
+
+//23
+int a = 2, b = 7, c = 2;
+if (a == b || b == c || a == c)
+    Console.WriteLine(true);
+else
+    Console.WriteLine(false);
+
+//24
+int a = 2, b = 7, c = -2;
+if ((a == 2 & b == 2) || (b == 2 & c == 2) || (a == 2 & c == 2))
+    Console.WriteLine(true);
+else
+    Console.WriteLine(false);
+
+//25
+int a = 2, b = 7, c = -2;
+if (a * a + b * b > c * c & b * b + c * c > a * a & a * a + c + c > b * b)
+    Console.WriteLine("1");
+else
+    Console.WriteLine("2");
+
+//26
+int a = 2, b = 7, c = -2;
+if (a % b == 0 || b % c == 0 || a % c == 0)
+    Console.WriteLine("1");
+else
+    Console.WriteLine("2");
+
+//27
+int a = 2, b = 7, c = -2;
+if (b - a == c - b)
+    Console.WriteLine(true);
+else
+    Console.WriteLine(false);
+
+//28
+int a = 2, b = 7, c = -2;
+if (b / a == c / b)
+    Console.WriteLine(true);
+else
+    Console.WriteLine(false);
+
+//29
+int a = -5, b = 2, c = 8, k;
+if (a < b)
+{
+
+    k = a;
+    a = b;
+    b = k;
+}
+if (a < c)
+{
+    k = a;
+    a = c;
+    c = k;
+}
+if (b < c)
+{
+    k = b;
+    b = c;
+    c = k;
+}
+Console.WriteLine($"{a},{b},{c}");
+
+//30
+int a = 2, b = 7, c = -2, k;
+if (a > b)
+{
+    k = a;
+    a = b;
+    b = k;
+}
+if (a > c)
+{
+    k = a;
+    a = c;
+    c = k;
+}
+if (b > c)
+{
+    k = b;
+    b = c;
+    c = k;
+}
+Console.WriteLine($"{a},{b},{c}");
+
+51
+bool p = false;
+int b, l, g;
+int t = int.Parse(Console.ReadLine());
+b = t % 10;
+g = t / 10;
+l = g % 10;
+l += g / 10;
+
+if (b == l)
+{
+    p = true;
+    Console.WriteLine(p);
+}
+else
+    Console.WriteLine($"{p} {t}");
+
+52
+bool p = false;
+int b, l, g;
+int t = int.Parse(Console.ReadLine());
+b = t % 10;
+t /= 10;
+g = t % 10;
+t /= 10;
+l = t % 10;
+
+if (b == g | b == l | l == g)
+{
+    p = true;
+    Console.WriteLine(p);
+}
+else
+    Console.WriteLine($"{p}");
+
+53
+decimal b, f, l, g, m;
+const decimal k = 225;
+decimal t = decimal.Parse(Console.ReadLine());
+f = t;
+
+if (t > k)
+{
+    b = f % 10;
+    f /= 10;
+    g = f % 10;
+    t /= 10;
+    l = f % 10;
+    m = t / (b + g + l);
+    Console.WriteLine(m);
+}
+else
+{
+    b = f % 10;
+    m = b / t;
+    Console.WriteLine(m);
+}
+
+54
+int b, l, g;
+int t = int.Parse(Console.ReadLine());
+
+b = t % 10;
+t /= 10;
+g = t % 10;
+t /= 10;
+l = t % 10;
+
+if (b > g)
+{
+    if (b > l)
+        Console.WriteLine(b);
+    else
+        Console.WriteLine(l);
+}
+else
+{
+    if (g > l)
+        Console.WriteLine(g);
+    else
+        Console.WriteLine(l);
+}
+
+55
+int b, l, g;
+int t = int.Parse(Console.ReadLine());
+
+b = t % 10;
+t /= 10;
+g = t % 10;
+t /= 10;
+l = t % 10;
+
+if (b < g)
+{
+    if (b < l)
+        Console.WriteLine(b);
+    else
+        Console.WriteLine(l);
+}
+else
+{
+    if (g < l)
+        Console.WriteLine(g);
+    else
+        Console.WriteLine(l);
+}
+
+56
+decimal b, f, l, g, m;
+decimal t = decimal.Parse(Console.ReadLine());
+f = t;
+b = f % 10;
+f /= 10;
+g = f % 10;
+f /= 10;
+l = f % 10;
+
+if (b > g)
+{
+    m = (b + g + l) / t;
+    Console.WriteLine(m);
+}
+else
+    Console.WriteLine(t);
+
+57--
+int l, f, b, g;
+decimal k;
+int t = int.Parse(Console.ReadLine());
+f = t;
+b = f % 10;
+f /= 10;
+g = f % 10;
+f /= 10;
+l = f % 10;
+
+if (t > 300)
+{
+    k = g / b;
+    Console.WriteLine(k);
+}
+else
+{
+    k = l / b;
+    Console.WriteLine(k);
+}
+
+58
+char f;
+int l, g, m;
+int t = int.Parse(Console.ReadLine());
+
+t /= 10;
+g = t % 10;
+t /= 10;
+l = t % 10;
+if (g + l < 5)
+    Console.WriteLine(f = 'a');
+else
+    Console.WriteLine(f = 'b');
+
+59
+int l, f, b, g;
+int t = int.Parse(Console.ReadLine());
+b = t % 10;
+t /= 10;
+g = t % 10;
+t /= 10;
+l = t % 10;
+
+if (b < g)
+{
+    f = g;
+    g = b;
+    b = f;
+}
+if (b < l)
+{
+    f = b;
+    b = l;
+    l = f;
+}
+if (g < l)
+{
+    f = g;
+    g = l;
+    l = f;
+}
+Console.WriteLine($"{b} {g} {l}");
+
+60
+int l, f, b, g;
+int t = int.Parse(Console.ReadLine());
+b = t % 10;
+t /= 10;
+g = t % 10;
+t /= 10;
+l = t % 10;
+
+if (b > g)
+{
+    f = g;
+    g = b;
+    b = f;
+}
+if (b > l)
+{
+    f = b;
+    b = l;
+    l = f;
+}
+if (g > l)
+{
+    f = g;
+    g = l;
+    l = f;
+}
+Console.WriteLine($"{b} {g} {l}");
+Console.ReadKey();
